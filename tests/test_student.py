341f2350ff72cc402f888ed6c442e13f519c4714
Huynh Ngoc Phien <hnphien12345@gmail.com>
2024-05-07 23:58:38 +0700

    Refactor test_create_document method in TestStudentCommonRepository

--- tests/test_student.py
+++ tests/test_student.py
@@ -9,19 +9,29 @@ class TestStudentCommonRepository(asynctest.TestCase):
         self.repo = StudentCommonRepository(collection_name="test")
         self.repo.collection = self.collection_mock
 
+        # Mock the insert_one and find_one methods with CoroutineMock
+        self.collection_mock.insert_one = asynctest.CoroutineMock()
+        self.collection_mock.find_one = asynctest.CoroutineMock()
+
     def tearDown(self) -> None:
-        self.collection_mock.reset_mock()
+        self.collection_mock.insert_one.reset_mock()
+        self.collection_mock.find_one.reset_mock()
 
     async def test_create_document(self):
         # Arrange
         excepted_data = {
             "_id": "123",
-            "name": "Test",
-            "email": "test@gmail.com",
-            "course": "Python",
-            "gpa": 20
+            "name": "Jane Doe",
+            "email": "jdoe@example.com",
+            "course": "Experiments, Science, and Fashion in Nanophotonics",
+            "gpa": 3.0,
         }
-        student = StudentModel(name="Test", email="test@gmail.com", course="Python", gpa=20)
+        student = StudentModel(
+            name="Jane Doe",
+            email="jdoe@example.com",
+            course="Experiments, Science, and Fashion in Nanophotonics",
+            gpa=3.0
+        )
         self.collection_mock.insert_one.return_value = asynctest.MagicMock(inserted_id="123")
         self.collection_mock.find_one.return_value = excepted_data
 
