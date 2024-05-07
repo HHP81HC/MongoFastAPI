34c9bb87b4654848b3b32fca37571549eec482d9
Huynh Ngoc Phien <hnphien12345@gmail.com>
2024-05-07 23:34:03 +0700

    chore: Update MongoDB URL to use IP address instead of localhost

--- src/utils/client.py
+++ src/utils/client.py
@@ -2,6 +2,6 @@
 import motor.motor_asyncio
 
 
-MONGODB_URL = "mongodb://localhost:27017/college?retryWrites=true&w=majority"
+MONGODB_URL = "mongodb://192.168.1.10:27017/college?retryWrites=true&w=majority"
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGODB_URL)
 db = client.college
